ee29096f48703f1ae86f99fadf4526c6731101f3
elberth.brandao <zelltr@hotmail.com>
2021-07-02 13:36:10 -0300

    Method sendEmail

--- src/main/java/com/elberthbrandao/integrations/controllers/EmailController.java
+++ src/main/java/com/elberthbrandao/integrations/controllers/EmailController.java
@@ -1,5 +1,6 @@
 package com.elberthbrandao.integrations.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,13 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.elberthbrandao.integrations.dto.EmailDTO;
+import com.elberthbrandao.integrations.services.EmailService;
 
 @RestController
 @RequestMapping(value = "/emails")
 public class EmailController {
+	
+	@Autowired
+	EmailService service;
 
 	@PostMapping
 	public ResponseEntity<Void> send(@RequestBody EmailDTO dto) {
+		service.sendEmail(dto);
 		return ResponseEntity.noContent().build();
 	}
 }
