960da9e8fd658d22b4bc4809c45e137b0e9f4016
elberth.brandao <zelltr@hotmail.com>
2021-07-03 03:53:54 -0300

    Sending email, exception handling

--- src/main/java/com/elberthbrandao/integrations/services/EmailService.java
+++ src/main/java/com/elberthbrandao/integrations/services/EmailService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.elberthbrandao.integrations.dto.EmailDTO;
+import com.elberthbrandao.integrations.services.exceptions.EmailException;
 import com.sendgrid.Method;
 import com.sendgrid.Request;
 import com.sendgrid.Response;
@@ -41,12 +42,12 @@ public class EmailService {
 
 			if (response.getStatusCode() >= 400 && response.getStatusCode() <= 500) {
 				LOG.error("Error sending e-mail: " + response.getBody());
-			} else {
-				LOG.info("E-mail sent! Status = " + response.getStatusCode());
+				throw new EmailException(response.getBody());
 			}
+			LOG.info("E-mail sent! Status = " + response.getStatusCode());
 
 		} catch (IOException e) {
-			e.printStackTrace();
+			throw new EmailException(e.getMessage());
 		}
 	}
 
---
+++ src/main/java/com/elberthbrandao/integrations/services/exceptions/EmailException.java
@@ -0,0 +1,8 @@
+package com.elberthbrandao.integrations.services.exceptions;
+
+public class EmailException extends RuntimeException {
+
+	public EmailException(String msg) {
+		super(msg);
+	}
+}
